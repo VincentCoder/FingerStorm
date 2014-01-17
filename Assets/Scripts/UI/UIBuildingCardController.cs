730cbcd3dab3e6edaff607e6a53f480b0ca00b64
chenyuanjy <chenyuanjy@verycd.com>
2014-01-17 10:30:19 +0800

    xx

--- Assets/Scripts/UI/UIBuildingCardController.cs
+++ Assets/Scripts/UI/UIBuildingCardController.cs
@@ -5,25 +5,30 @@ public class UIBuildingCardController : MonoBehaviour
 {
 	private GameObject building;
 	
-	void Start () 
+	void Start ()
 	{
+	    UIEventListener.Get(this.gameObject).onPress = HandleEvent_Press;
 		UIEventListener.Get(this.gameObject).onDrag = HandleEvent_Drag;
 		UIEventListener.Get(this.gameObject).onDrop = HandleEvent_Drop;
 	}
+
+    private void HandleEvent_Press(GameObject go, bool state)
+    {
+        building = (GameObject)Instantiate(Resources.Load("GameScene/Building"));
+        building.transform.position = UICamera.lastHit.point;
+    }
 	
 	private void HandleEvent_Drag( GameObject eventObj, Vector2 delta )
 	{
-		if(building == null)
+		if(building != null)
 		{
-			building = (GameObject)Instantiate(Resources.Load("GameScene/Building"));
+            building.transform.position += ((Vector3)delta * GameObject.Find("UIRoot").GetComponent<UIRoot>().pixelSizeAdjustment);
 		}
-		building.transform.position = new Vector3(UICamera.currentTouch.pos.x + 480, UICamera.currentTouch.pos.y + 320, 0);
-		//UICamera.currentTouch.
 	}
 	
 	private void HandleEvent_Drop(GameObject go, GameObject draggedObject)
 	{
-		
+		Debug.Log("Drop");
 	}
 	
 	// Update is called once per frame
